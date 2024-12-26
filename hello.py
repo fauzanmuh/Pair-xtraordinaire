b5f0a803c0400aed816accf64a5c179fd95e8d2a
Muhammad Fauzan <60720670+fauzanmuh@users.noreply.github.com>
2024-12-26 11:17:26 +0700

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,10 @@
-print['Hello World']
+import math
+
+def count_circle();
+print[" ---- Count Circle Program ---- "]
+try:
+  jari_jari = float(input("Masukkan Jari-Jari: "))
+
+if jari_jari < 0:
+            print("Jari-jari tidak boleh negatif!")
+            return
